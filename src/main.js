b6df85f970526b41c2863a3fa8a722e54fa747e0
S-H-Y-A <74596628+S-H-Y-A@users.noreply.github.com>
2022-12-12 18:34:34 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
+import VueMeta from 'vue-meta'
 import App from './App.vue'
 
 import '@/styles/global/reset.scss'
 import '@/styles/global/app.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMeta, {
+  keyName: 'head'
+})
+
 
 new Vue({
   render: h => h(App)
